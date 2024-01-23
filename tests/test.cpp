601cccb70d2b41dc9c90b8b396da101039a73b7d
left <107749872+0xleft@users.noreply.github.com>
2024-01-23 15:23:23 +0100

    fucking fixed

--- tests/test.cpp
+++ tests/test.cpp
@@ -8,13 +8,32 @@
 //#include <engine/evaluator.h>
 #include <chess/src/include.hpp>
 
-int main() {
-    chess::Board board = chess::Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
-    std::cout << sizeof(chess::attacks::BishopAttacks[0]) << std::endl;
+using namespace chess;
 
-    chess::Movelist moves;
-    chess::movegen::legalmoves(moves, board);
-    std::cout << moves.size();
+uint64_t perft(Board& board, int depth) {
+    Movelist moves;
+    movegen::legalmoves(moves, board);
+
+    if (depth == 1) {
+        return moves.size();
+    }
+
+    uint64_t nodes = 0;
+
+    for (int i = 0; i < moves.size(); i++) {
+        const auto move = moves[i];
+        board.makeMove(move);
+        nodes += perft(board, depth - 1);
+        board.unmakeMove(move);
+    }
+
+    return nodes;
+}
+
+int main() {
+    Board board = Board("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+
+    std::cout << perft(board, 4);
 
     return 0;
 }
